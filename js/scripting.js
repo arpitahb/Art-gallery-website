29f47445e3da66a6226c09a2017a8005b8061a01
Khusnasin <khusnasinsultana@gmail.com>
2023-07-05 03:43:17 +0530

    Second edit

--- js/scripting.js
+++ js/scripting.js
@@ -1,7 +1,65 @@
 function openModal(modalId) {
-    document.getElementById(modalId).style.display = "block";
-  }
+  document.getElementById(modalId).style.display = "block";
+}
 
-  function closeModal(modalId) {
-    document.getElementById(modalId).style.display = "none";
-  }
+function closeModal(modalId) {
+  document.getElementById(modalId).style.display = "none";
+}
+
+
+/*function toggleHiddenObjects(){
+  var hiddenImages = document.querySelectorAll('.hidden');
+  hiddenImages.forEach(function (image) {
+    image.classList.remove('hidden');
+  });
+}*/
+
+function toggleHiddenObjects() {
+  var hiddenObjects = document.querySelectorAll('.row.row.bs.hidden');
+  var viewMoreBtn = document.getElementById('viewMoreBtn');
+
+  if (hiddenObjects.length > 0) {
+    for (var i = 0; i < hiddenObjects.length; i++) {
+      if (i < 3) {
+        hiddenObjects[i].classList.remove('hidden');
+      } else {
+        hiddenObjects[i].style.display = 'none';
+      }
+    }
+
+    if (hiddenObjects.length <= 3) {
+      viewMoreBtn.innerHTML = 'View Less';
+    }
+  } else {
+    var allObjects = document.querySelectorAll('.row.row.bs');
+    for (var j = 3; j < allObjects.length; j++) {
+      allObjects[j].classList.add('hidden');
+    }
+    viewMoreBtn.innerHTML = 'View More';
+      }
+    }
+
+    /*var hiddenObjects = document.querySelectorAll('.hiddenSection .hidden');
+    var viewMoreButton = document.getElementById('viewMoreButton');
+    var isExpanded = false;
+
+    function toggleHiddenSection() {
+      for (var i = 0; i < hiddenObjects.length; i++) {
+        hiddenObjects.forEach(function (image) {
+          image.classList.remove('hidden');
+        });
+    }
+    
+    var visibleObjects = document.querySelectorAll('#hiddenSection .hidden');
+        if (visibleObjects.length === 0) {
+            viewMoreButton.style.display = 'none';
+        }
+        
+        if (!isExpanded) {
+            viewMoreButton.textContent = 'View Less';
+            isExpanded = true;
+        } else {
+            viewMoreButton.textContent = 'View More';
+            isExpanded = false;
+        }
+    }*/
